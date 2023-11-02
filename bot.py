944dc68616471757b15c9c3961bd2ea810940e67
chumachenko <d.chumachenko@team.ff-group.org>
2023-11-02 13:37:08 +0100

    Fixed imports, deleted media folder

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
-import config
-import database.service as db
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import config
+import database.service as db
 import io
 
 bot = Bot(token=config.BOT_TOKEN)
