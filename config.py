345b805b1fd928ffc6df31fbcb9c3dc5f9f3258f
chumachenko <d.chumachenko@team.ff-group.org>
2023-11-03 11:07:13 +0100

    all files

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = '6640109431:AAGwr1B6kNF20U67ysgkFD4fEStpp8vmJ34'
+BOT_TOKEN = '6694693885:AAECgaOBHQ__d4_COXQryRDbXo102-5rV7w'
 ADMINS = [993868802, 741599680]
 DB_URL = "postgresql://postgres:postgres@localhost/mentors_bot"
