7d2326c04f58b514d8cd0d27794f60963485e1e8
chumachenko <d.chumachenko@team.ff-group.org>
2023-11-03 16:11:58 +0100

    heroku fix

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = '6694693885:AAECgaOBHQ__d4_COXQryRDbXo102-5rV7w'
 ADMINS = [993868802, 741599680]
-APP_URL = "https://czech-uni-mentor-bot-8394ca5fd5e9.herokuapp.com/" + BOT_TOKEN
+APP_URL = "https://czech-uni-mentor-bot-fa8629801009.herokuapp.com/" + BOT_TOKEN
 DB_URL = "postgresql://postgres:postgres@localhost/mentors_bot"
