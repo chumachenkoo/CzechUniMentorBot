d2d1459176c07587f6b6728b4670a975bade21b3
chumachenko <d.chumachenko@team.ff-group.org>
2023-11-07 12:17:48 +0100

    Remove config from repository

--- config.py
+++
@@ -1,5 +0,0 @@
-BOT_TOKEN = '6694693885:AAECgaOBHQ__d4_COXQryRDbXo102-5rV7w'
-ADMINS = [993868802, 741599680]
-APP_URL = "https://czech-uni-mentor-bot-fa8629801009.herokuapp.com/" + BOT_TOKEN
-DB_URL = "postgresql://imwgptbeuiypxa:4e867bb0d19daf0a6565b2068576ad245b07e1837771fe50eaec5914299c70fc@ec2-54-195-24-35.eu-west-1.compute.amazonaws.com:5432/db6insd0dm535d"
-
